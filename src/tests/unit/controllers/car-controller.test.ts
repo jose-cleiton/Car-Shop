6e7c03b9b758fecd7e00ddb1fddd75041bc0cbef
jose-cleiton <cleitons835>
2022-09-18 19:03:20 -0300

    Test controller

--- src/tests/unit/controllers/car-controller.test.ts
+++ src/tests/unit/controllers/car-controller.test.ts
@@ -1,58 +1,62 @@
-// // template para criação dos testes de cobertura da camada de model
+// template para criação dos testes de cobertura da camada de model
 
-// import { Request, Response } from 'express';
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// import Car from '../../../models/CarModel';
-// import ServiceCar from '../../../services/ServiceCar';
-// import { Model } from 'mongoose';
-// const { expect } = chai;
-// import {DTOMock, MockCar} from '../../mocks/CarsMock';
-// import CarController from '../../../controllers/CarController';
-// describe('Contoller', () => {
+import { Request, Response } from 'express';
+import * as sinon from 'sinon';
+import chai from 'chai';
+import Car from '../../../models/CarModel';
+import ServiceCar from '../../../services/ServiceCar';
+import { Model } from 'mongoose';
+const { expect } = chai;
+import {DTOMock, MockCar} from '../../mocks/CarsMock';
+import CarController from '../../../controllers/CarController';
+describe('Contoller', () => {
 
-//   const car = new Car();
-//   const carService = new ServiceCar(car);
-//   const carContrtroller = new CarController(carService);
+  const car = new Car();
+  const carService = new ServiceCar(car);
+  const carContrtroller = new CarController(carService);
 
-//   let req = {} as Request;
-//   let res = {} as Response;
+  let req = {} as Request;
+  let res = {} as Response;
 
-//   before(async () => {
-//     // aqui você pode fazer alguma configuração antes de rodar os testes
-//     sinon.stub(Model, 'create').resolves(MockCar);
-//     sinon.stub(Model, 'find').resolves([]);
-//     sinon.stub(Model, 'findOne').resolves(MockCar);
-//     sinon.stub(Model, 'findByIdAndUpdate').resolves(MockCar);
-//     sinon.stub(Model, 'findByIdAndDelete').resolves(MockCar);
-//     res.status = sinon.stub().returns(res);
-//     res.json = sinon.stub().returns(res);
+  before(async () => {
+    // aqui você pode fazer alguma configuração antes de rodar os testes
+    sinon.stub(Model, 'create').resolves(MockCar);
+    sinon.stub(Model, 'find').resolves([]);
+    sinon.stub(Model, 'findOne').resolves(MockCar);
+    sinon.stub(Model, 'findByIdAndUpdate').resolves(MockCar);
+    sinon.stub(Model, 'findByIdAndDelete').resolves(MockCar);
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+    req.body = DTOMock
      
-//   });
+  });
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+  after(()=>{
+    sinon.restore();
+  })
 
-//   describe('create', () => {
+  describe('create', () => {
       
-//       it('Em caso de sucesso', async () => {
-//         req.body = DTOMock;
-//         await carContrtroller.postCar(req, res);
-//         expect(res.status(201)).to.be.true;
-//         expect(res.json(MockCar)).to.be.true;
-//       });
-  
-//       it('Em caso de erro', async () => {
-//         try {
-//           await carContrtroller.postCar(req, res);
-//         } catch (error) {
-//           expect(error)
-          
-//         }
-//       });
+   it('should create a new car', async () => {
+    
+      await carContrtroller.postCar(req, res);
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+
+      
+    })
+
+    it('should read ', async () => {
+      req.params = { id: "632383aaea59c2b5dc96346b" }
+      await carContrtroller.getCars(req, res);
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+    })
+
+    it('should read one ', async () => {
+      await carContrtroller.getCarById(req, res);
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+    });
         
-//   });
+  });
 
 
 
