7e8a5d967c7d57b8afe97b92af8c050949d07336
jose-cleiton <cleitons835>
2022-09-18 09:34:53 -0300

    Tirando exportação da forma padrão

--- src/interfaces/IVehicle .ts
+++ src/interfaces/IVehicle .ts
@@ -1,13 +1,10 @@
 import { z } from 'zod';
 
-const vehicleZodSchema = z.object({
+export const vehicleZodSchema = z.object({
   model: z.string().min(3),
   year: z.number().int().gte(1900).lte(2022),
   color: z.string().min(3),
   status: z.boolean().optional(),
   buyValue: z.number().int(),
 });
-type IVehicle = z.infer<typeof vehicleZodSchema>;
-
-export default IVehicle;
-export { vehicleZodSchema };
+export type IVehicle = z.infer<typeof vehicleZodSchema>;
