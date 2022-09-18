a53d8d6e6c65ff0d0e5544e573ffa0f1d09a0f29
jose-cleiton <cleitons835>
2022-09-18 09:48:22 -0300

    reestruturando

--- src/interfaces/IVehicle .ts
+++ src/interfaces/IVehicle .ts
@@ -1,10 +1,11 @@
 import { z } from 'zod';
 
-export const vehicleZodSchema = z.object({
+export const VehicleZodSchema = z.object({
   model: z.string().min(3),
-  year: z.number().int().gte(1900).lte(2022),
+  year: z.number().min(1900).max(2022),
   color: z.string().min(3),
-  status: z.boolean().optional(),
+  status: z.boolean().optional(), 
   buyValue: z.number().int(),
 });
-export type IVehicle = z.infer<typeof vehicleZodSchema>;
+  
+export type IVehicle = z.infer<typeof VehicleZodSchema>;
